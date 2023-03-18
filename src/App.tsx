bc11fb442f0215c2c2298c7e80022a69bdde50ea
christian <calbujar@educri.com.pe>
2023-03-18 17:58:56 -0500

    first upload project

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br></br>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          En construcción.!
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
